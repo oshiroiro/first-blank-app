3caa1c6fd53737621f4c7521b92ac2c90a202d47
おおしろ <yuumi.oshiro@gmail.com>
2024-07-19 13:43:01 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 こんにちは！ようこそ！")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
