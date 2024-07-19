93a2e4207e301550f6fa65f84e7fde662b230ef1
おおしろ <yuumi.oshiro@gmail.com>
2024-07-19 13:44:17 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ st.title("🎈 こんにちは！ようこそ！")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+age = st.slider('年齢を選択してください', 0, 100, 25)
+st.write(f'あなたの年齢は {age} 歳です')
